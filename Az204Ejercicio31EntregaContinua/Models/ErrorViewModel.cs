6705a2405da00350d0a2a3ffdedd487cb18ee2ac
jesus <jd_rico@yahoo.es>
2024-04-02 09:58:43 +0200

    Agregar archivos de proyecto.

---
+++ Az204Ejercicio31EntregaContinua/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Az204Ejercicio31EntregaContinua.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
